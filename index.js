8cc51ef07da721da584c24498099c8bd0a4bc5c0
JoanMburu <joan.wangui@student.moringaschool.com>
2024-04-02 23:35:30 +0300

    creates arrow functions

---
+++ index.js
@@ -0,0 +1,12 @@
+const divide = (parameter1, parameter2) =>{
+    return parameter1/parameter2;
+}
+divide (2000,100);
+
+const square = (x)=> {
+    return (x*2);
+}
+
+const add = (a,b) =>{
+    return a+b;
+}
